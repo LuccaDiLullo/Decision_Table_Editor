ed77dcad30a9da23039333295756ed359aae9cbb
Julien Lefebvre <jul.lefebvre19@gmail.com>
2023-10-22 18:39:03 -0400

    Project files init, requirements.txt and gitignore, removal of unwanted venv in the repository

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\yazan\General_workspace\git\Decision_Table_Editor\venv
+set VIRTUAL_ENV=C:\Users\j-l19\OneDrive\Documents\McGill\Term 7 (Fall 2023)\ECSE-458\Project\Decision_Table_Editor\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
