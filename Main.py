a11bd5b6da1d7e16fb07f1e4d5e15fcdfcbc11b9
Yazan Saleh <77689402+YazanAS@users.noreply.github.com>
2023-10-25 18:12:16 -0400

    Update Main.py for CI fix

--- Main.py
+++ Main.py
@@ -3,11 +3,11 @@ import functools
 from pywebio.input import input, select, input_group
 from pywebio.output import clear, put_button, put_text, put_table
 
-# Initialize conditions and actions for the table
+# Initialize default conditions and actions for the table
 conditions = ["Condition 1"]
 actions = ["Action 1"]
 
-# Initial table data structure
+# Initialize default table structure
 table_data = {
     "table_name": "My Decision Table",
     "num_conditions": 1,
@@ -23,40 +23,41 @@ def main():
 
 
 def create_table():
-    """Prompt user for table name and display the table."""
-    table_data["table_name"] = input("How should we name this decision table?")
+    """Capture table name and display it."""
+    table_name = "How should we name this decision table?"
+    table_data["table_name"] = input(table_name)
     display_table()
 
 
 def add_condition():
-    """Add a new condition variable to the table."""
+    """Add new condition to table."""
     global table_data, conditions
-
     condition_name = input("Enter a name for the condition:")
     table_data["num_conditions"] += 1
     conditions.append(condition_name)
-    table_data["headers"].insert(table_data["num_conditions"], condition_name)
+    position = table_data["num_conditions"]
+    table_data["headers"].insert(position, condition_name)
 
+    # Generate new table combinations
     conditions_combinations = generate_combinations(table_data["num_conditions"])
     new_table_data = copy.deepcopy(conditions_combinations)
-    for index in range(len(conditions_combinations)):
-        new_table_data[index].insert(0, index + 1)
-        for _ in range(table_data["num_actions"]):
-            new_table_data[index].append(False)
+    for idx, combo in enumerate(conditions_combinations):
+        new_table_data[idx].insert(0, idx + 1)
+        new_table_data[idx].extend([False] * table_data["num_actions"])
 
     table_data["data"] = copy.deepcopy(new_table_data)
     display_table()
 
 
 def add_action():
-    """Add a new action column to the table."""
+    """Add new action to table."""
     global table_data, actions
-
     action_name = input("Enter a name for the action:")
     table_data["headers"].append(action_name)
     actions.append(action_name)
     table_data["num_actions"] += 1
 
+    # Add False for new action in each row
     for row in table_data["data"]:
         row.append(False)
 
@@ -64,7 +65,7 @@ def add_action():
 
 
 def add_rule():
-    """Add a new rule to the table based on user inputs."""
+    """Capture rule from user and update table."""
     inputs = input_group("Add Rule", [
         select("If [condition]", options=conditions, name="rule_condition"),
         select("is [boolean]", options=[True, False], name="rule_boolean"),
@@ -72,30 +73,34 @@ def add_rule():
         select("is [decision]", options=[True, False], name="rule_decision")
     ])
 
-    i_c = next((i for i, header in enumerate(table_data["headers"]) if header == inputs['rule_condition']), None)
-    i_a = next((i for i, header in enumerate(table_data["headers"]) if header == inputs['rule_action']), None)
+    condition_index = table_data["headers"].index(inputs['rule_condition'])
+    action_index = table_data["headers"].index(inputs['rule_action'])
 
-    for i_r, row in enumerate(table_data["data"]):
-        if row[i_c] == inputs["rule_boolean"]:
-            if row[i_a] != inputs["rule_decision"]:
-                toggle_action(i_r, i_a)
+    for row_index, row in enumerate(table_data["data"]):
+        if row[condition_index] == inputs["rule_boolean"]:
+            if row[action_index] != inputs["rule_decision"]:
+                toggle_action(row_index, action_index)
 
-    put_text(f"Rule: if {inputs['rule_condition']} = {inputs['rule_boolean']}, "
-             f"then {inputs['rule_action']} = {inputs['rule_decision']}")
+    rule_display = (f"Rule: if {inputs['rule_condition']} = "
+                    f"{inputs['rule_boolean']}, then {inputs['rule_action']} = "
+                    f"{inputs['rule_decision']}")
+    put_text(rule_display)
 
 
 def display_table():
-    """Display the decision table with its current data."""
+    """Display the current decision table."""
     table_array = copy.deepcopy(table_data["data"])
 
+    # Convert boolean values to buttons
     for i in range(len(table_data["data"])):
         for j in range(len(table_data["data"][i])):
             if j > table_data["num_conditions"]:
-                table_array[i][j] = put_button(
+                btn = put_button(
                     table_array[i][j],
                     onclick=functools.partial(toggle_action, i, j),
                     color=get_color(table_array[i][j])
                 )
+                table_array[i][j] = btn
 
     clear()
     put_text(table_data["table_name"])
@@ -106,18 +111,18 @@ def display_table():
 
 
 def toggle_action(row, column):
-    """Toggle the value in an action column."""
+    """Change action's boolean value in the table."""
     table_data["data"][row][column] = not table_data["data"][row][column]
     display_table()
 
 
 def get_color(value):
-    """Return color based on boolean value."""
+    """Get button color based on its value."""
     return 'success' if value else 'danger'
 
 
 def generate_combinations(n):
-    """Generate all 2^n possible combinations of n boolean variables."""
+    """Generate 2^n boolean combinations."""
     if n == 0:
         return [[]]
     smaller_combinations = generate_combinations(n - 1)
